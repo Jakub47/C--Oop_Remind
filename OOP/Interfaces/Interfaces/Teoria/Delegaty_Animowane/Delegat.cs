04744b909d1f0a762f31369f0e50e1c3e02b5c47
Jakub47 <jakub7447@gmail.com>
2018-08-11 21:55:54 +0200

    Delegaty_Animowane metody

--- OOP/Interfaces/Interfaces/Teoria/Delegaty_Animowane/Delegat.cs
+++ OOP/Interfaces/Interfaces/Teoria/Delegaty_Animowane/Delegat.cs
@@ -9,15 +9,61 @@ namespace Interfaces.Teoria.Delegaty_Animowane
     class Delegat
     {
         private delegate int TypDelekacji(int a);
+        private delegate void Writer(string message);
 
-        public static void Main(string[] args)
+
+        public class Logger
+        {
+            private readonly Delegat _delegat;
+
+            public Logger(Delegat delegat)
+            {
+                this._delegat = delegat;
+            }
+
+            public void WriteMessage(string message)
+            {
+                Console.WriteLine(message + "Cs 1.6\n");
+            }
+
+            public void WriteMessage2(string message2)
+            {
+                Console.WriteLine(message2 + "CS Global");
+            }
+        }
+
+
+        public static void main(string[] args)
         {
             var a = new TypDelekacji(Kwadrat);
+            var writer = new Writer(new Logger(new Delegat()).WriteMessage);
 
-            Console.WriteLine("-------------------------------");
-            a += InnaMetoda;
+            //TypDelekacji obiektDelegacj = delegate (int x)
+            //{
+            //    Console.WriteLine("Wywa;mp metodę anonimową");
+            //    return x * x;
+            //};
+
+            //===
+            TypDelekacji obiektDelegacj = (int x) =>
+            {
+                Console.WriteLine("Wyrazenie lamda");
+                return x * x;
+            };
+
+            TypDelekacji obiektDelegacji = x => x * x;
+            Console.WriteLine("5*5=" + obiektDelegacji(5));
+
+            //obiektDelegacj += Kwadrat;
+
+            Console.WriteLine(obiektDelegacj(2));
+
+            //a += InnaMetoda;
+            //writer += new Logger(new Delegat()).WriteMessage2;
+
+            //writer("Kaboom2");
             //Zostaną wywołane wszystkie metody zarejestrowane w delegacie a!
-            Console.WriteLine(a(10));
+            //Console.WriteLine(a(10));
             
             Console.ReadKey();
         }
---
+++ OOP/Interfaces/Interfaces/Teoria/Delegaty_Animowane/Delegaty.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Teoria.Delegaty_Animowane
+{
+    public class Delegaty
+    {
+        protected delegate int Delegat1(int arg1, int arg2);
+    }
+
+    class Program : Delegaty
+    { 
+        public static void Main(string[] args)
+        {
+            Delegat1 dodawanie1 = delegate (int arg1, int arg2)
+            {
+                return arg1 + arg2;
+            };
+            Console.WriteLine("Wynik dodwania " + dodawanie1(4,2));
+
+            Delegat1 odejmowanie = (int c, int d) => { return c - d; };
+
+            Console.WriteLine("Wynik dodwania " + odejmowanie(15,5));
+        }
+
+    }
+}
